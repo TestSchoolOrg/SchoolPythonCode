0941086a131d1166d310380acffeeb6ee6fbbd58
CatBackG <115424457+CatBackG@users.noreply.github.com>
2025-10-14 20:50:03 +0500

    Update RSA.py

--- RSA.py
+++ RSA.py
@@ -1,1 +1,16 @@
-f
+alf = ["а","б","в","г","д","е","ё","ж","з","и","й","к","л","м","н","о","п","р","с","т","у","ф","х","ц","ч","ш","щ","ъ","ы","ь","э","ю","я"]
+
+pre = []
+
+word = list(input("Введите слово: "))
+formatted_word = "".join(word)
+print(f"Изначальное слово: {formatted_word}")
+
+for i in range(len(word)):
+    u = alf.index(word[i]) + 1
+    y = u ** 7
+    o = y % 33
+    pre.append(alf[o])
+
+result = "".join(pre)
+print(f"Зашифрованное слово: {result}")
